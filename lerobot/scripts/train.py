fb6db5696860c06f3ae481031fef40eb25c7cd40
Cris Lima Froes <cris.lima.froes@gmail.com>
2025-03-03 17:45:55 -0300

    Update train.py

--- lerobot/scripts/train.py
+++ lerobot/scripts/train.py
@@ -67,8 +67,6 @@ def update_policy(
     lock=None,
 ) -> tuple[MetricsTracker, dict]:
     start_time = time.perf_counter()
-    for k in batch.keys():
-        batch[k] = batch[k].to(torch.float16)
     device = get_device_from_parameters(policy)
     policy.train()
     with torch.autocast(device_type=device.type) if use_amp else nullcontext():
